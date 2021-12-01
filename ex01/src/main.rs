6ff52390db822305fb776c2a6bd3d19046d489da
Pierrick Bouvier <pierrick.bouvier@allegrodvt.com>
2021-12-01 15:55:10 +0100

    ex1, part1

--- ex01/src/main.rs
+++ ex01/src/main.rs
@@ -1,3 +1,16 @@
+fn part1(input: &Vec<i32>) {
+    let answer = &input
+                .windows(2)
+                .filter(|i| i[1] > i[0])
+                .count();
+    println!("part1: {}", answer)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_str = include_str!("../input.txt");
+    let input: Vec<i32> = input_str
+                  .lines()
+                  .map(|i| i.parse::<i32>().unwrap())
+                  .collect();
+    part1(&input);
 }
