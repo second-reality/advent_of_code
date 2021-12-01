f0f988ef3232412b323f5dbad5ce4955c0dd4b7f
Pierrick Bouvier <pierrick.bouvier@allegrodvt.com>
2021-12-01 18:30:11 +0100

    part2

--- ex01/src/main.rs
+++ ex01/src/main.rs
@@ -1,16 +1,18 @@
-fn part1(input: &Vec<i32>) {
-    let answer = &input
-                .windows(2)
-                .filter(|i| i[1] > i[0])
-                .count();
-    println!("part1: {}", answer)
+fn part1(input: &[i32]) -> usize {
+    input.windows(2).filter(|i| i[1] > i[0]).count()
+}
+
+fn part2(input: &[i32]) -> usize {
+    let all_sums: Vec<i32> = input.windows(3).map(|i| i[0] + i[1] + i[2]).collect();
+    part1(&all_sums)
 }
 
 fn main() {
     let input_str = include_str!("../input.txt");
     let input: Vec<i32> = input_str
-                  .lines()
-                  .map(|i| i.parse::<i32>().unwrap())
-                  .collect();
-    part1(&input);
+        .lines()
+        .map(|i| i.parse::<i32>().unwrap())
+        .collect();
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
