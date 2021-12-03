f227d96dc9ed4a6d5668bfbd0743897ba38602b0
Axel Ricard <axel.ricard@allegrodvt.com>
2021-12-03 12:20:46 +0100

    day1: refactor part1

--- axel/day1/src/main.rs
+++ axel/day1/src/main.rs
@@ -1,15 +1,11 @@
-// Gotta use nightly channel to build :)  
-#![feature(stdin_forwarders)]
-
-use std::io;
-
 fn main() {
+    let input = include_str!("../sample");
+
     let mut increase_cmpt : i32 = 0;
     let mut pred_floor_depth = i32::MAX;
 
-    for line in io::stdin().lines() {
-        let floor_depth = line.unwrap()
-                               .parse::<i32>().unwrap();
+    for line in input.lines() {
+        let floor_depth = line.parse::<i32>().unwrap();
 
         if floor_depth > pred_floor_depth {
             increase_cmpt += 1;
