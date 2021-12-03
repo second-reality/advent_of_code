7d89965b60d3346a13d1aef19a5fe20b6f0d3bb7
Pierrick Bouvier <pierrick.bouvier@allegrodvt.com>
2021-12-03 10:12:56 +0100

    ex3 part2

--- pierrick/03/src/main.rs
+++ pierrick/03/src/main.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
-fn part1(input: &str) -> i32 {
+fn get_scores(input: &[String]) -> Vec<HashMap<char, i32>> {
     let mut scores: Vec<HashMap<char, i32>> = vec![];
 
-    for l in input.lines() {
+    for l in input {
         assert!(scores.is_empty() || l.len() == scores.len());
         scores.resize(l.len(), HashMap::new());
 
@@ -12,21 +12,87 @@ fn part1(input: &str) -> i32 {
         }
     }
 
-    let mut gamma = 0;
-    let mut epsilon = 0;
+    scores
+}
+
+fn get_score(c: char, score: &HashMap<char, i32>) -> i32 {
+    *score.get(&c).unwrap_or(&0)
+}
+
+fn get_most_frequent_char(score: &HashMap<char, i32>) -> char {
+    if get_score('1', score) >= get_score('0', score) {
+        '1'
+    } else {
+        '0'
+    }
+}
+
+fn get_least_frequent_char(score: &HashMap<char, i32>) -> char {
+    if get_score('1', score) >= get_score('0', score) {
+        '0'
+    } else {
+        '1'
+    }
+}
+
+fn str_bit_to_i32(s: String) -> i32 {
+    let mut res = 0;
+    for c in s.chars() {
+        res <<= 1;
+        if c == '1' {
+            res += 1;
+        }
+    }
+    res
+}
+
+fn part1(scores: &[HashMap<char, i32>]) -> i32 {
+    let mut gamma = String::new();
+    let mut epsilon = String::new();
     for s in scores {
-        let most_frequent_bit = if s[&'0'] > s[&'1'] { 0 } else { 1 };
-        let least_frequent_bit = !most_frequent_bit & 0x1;
-        gamma <<= 1;
-        epsilon <<= 1;
-        gamma += most_frequent_bit;
-        epsilon += least_frequent_bit;
+        gamma.push(get_most_frequent_char(s));
+        epsilon.push(get_least_frequent_char(s));
     }
 
-    gamma * epsilon
+    str_bit_to_i32(gamma) * str_bit_to_i32(epsilon)
+}
+
+fn find_unique_entry(
+    idx: usize,
+    input: &[String],
+    select_char: fn(&HashMap<char, i32>) -> char,
+) -> String {
+    assert!(!input.is_empty());
+    if input.len() == 1 {
+        return input[0].clone();
+    }
+
+    let scores = get_scores(input);
+    let c = select_char(&scores[idx]);
+    let selected: Vec<String> = input
+        .iter()
+        .filter(|s| s.chars().nth(idx).unwrap() == c)
+        .cloned()
+        .collect();
+    find_unique_entry(idx + 1, &selected, select_char)
+}
+
+fn part2(input: &[String]) -> i32 {
+    let oxygen = find_unique_entry(0, input, |s| get_most_frequent_char(s));
+    let co2 = find_unique_entry(0, input, |s| get_least_frequent_char(s));
+    str_bit_to_i32(oxygen) * str_bit_to_i32(co2)
+}
+
+fn get_input() -> Vec<String> {
+    include_str!("../input.txt")
+        .lines()
+        .map(String::from)
+        .collect()
 }
 
 fn main() {
-    let input = include_str!("../input.txt");
-    println!("{}", part1(input));
+    let input = get_input();
+    let scores = get_scores(&input);
+    println!("{}", part1(&scores));
+    println!("{}", part2(&input));
 }
