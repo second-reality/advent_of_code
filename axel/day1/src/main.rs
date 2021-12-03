eb3184231867f26eb9e4704145d876cb7ddb1cc4
Axel Ricard <axel.ricard@allegrodvt.com>
2021-12-03 12:37:43 +0100

    day1 part2 done

--- axel/day1/src/main.rs
+++ axel/day1/src/main.rs
@@ -1,9 +1,11 @@
+
 fn main() {
     let input = include_str!("../sample");
 
     let mut increase_cmpt : i32 = 0;
     let mut pred_floor_depth = i32::MAX;
 
+    // Part 1
     for line in input.lines() {
         let floor_depth = line.parse::<i32>().unwrap();
 
@@ -13,6 +15,21 @@ fn main() {
         pred_floor_depth = floor_depth;
     }
 
-    println!("{}", increase_cmpt);
+    let mut increase_cmpt_part2 : i32 = 0;
+    let mut pred_floor_depth_part2 = i32::MAX;
+    // Part 2
+    for window in input.lines().collect::<Vec<&str>>().windows(3)
+    {
+        let sum : i32 = window.iter().map(|s| s.parse::<i32>().unwrap()).sum();
+        if sum > pred_floor_depth_part2
+        {
+            increase_cmpt_part2 += 1;
+        }
+        pred_floor_depth_part2 = sum;
+    }
+
+
+    println!("part 1 : {}", increase_cmpt);
+    println!("part 2 : {}", increase_cmpt_part2);
 }
 
