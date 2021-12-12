0ea6c7558f8618c86e948c99cb9670a7dfe6eb34
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2021-12-12 18:46:26 +0100

    part2

--- pierrick/12/src/main.rs
+++ pierrick/12/src/main.rs
@@ -10,28 +10,21 @@ impl CaveMap {
         let left = String::from(left);
         let right = String::from(right);
         self.paths
-            .entry(left.clone())
+            .entry(left)
             .or_insert_with(HashSet::new)
-            .insert(right.clone());
-        self.paths
-            .entry(right)
-            .or_insert_with(HashSet::new)
-            .insert(left);
+            .insert(right);
     }
 
     fn is_small_cave(name: &str) -> bool {
         name.chars().next().unwrap().is_lowercase()
     }
 
-    fn explore_paths(&self, start: &str, end: &str) -> HashSet<Vec<String>> {
-        self.recursively_find_path(start, end, &[])
-    }
-
-    fn recursively_find_path(
+    fn explore_paths(
         &self,
         start: &str,
         end: &str,
         visited: &[String],
+        can_visit: fn(&str, &[String]) -> bool,
     ) -> HashSet<Vec<String>> {
         let mut visited = visited.to_owned();
         visited.push(String::from(start));
@@ -44,11 +37,10 @@ impl CaveMap {
             let mut res = HashSet::new();
 
             for dest in self.paths[start].iter() {
-                if Self::is_small_cave(dest) && visited.iter().any(|c| c == dest) {
-                    continue;
+                if can_visit(dest, &visited) {
+                    let new_paths = self.explore_paths(dest, end, &visited, can_visit);
+                    res.extend(new_paths);
                 }
-                let new_paths = self.recursively_find_path(dest, end, &visited);
-                res.extend(new_paths);
             }
 
             res
@@ -64,13 +56,47 @@ fn get_input(s: &str) -> CaveMap {
         let right = connection[1];
 
         res.add_connection(left, right);
+        res.add_connection(right, left);
     }
     res
 }
 
 fn part1(s: &str) -> usize {
     let cave = get_input(s);
-    cave.explore_paths("start", "end").len()
+
+    fn can_visit(dest: &str, visited: &[String]) -> bool {
+        if CaveMap::is_small_cave(dest) {
+            let was_seen = visited.iter().any(|cave| cave == dest);
+            !was_seen
+        } else {
+            true
+        }
+    }
+    cave.explore_paths("start", "end", &[], can_visit).len()
+}
+
+fn part2(s: &str) -> usize {
+    let cave = get_input(s);
+
+    fn can_visit(dest: &str, visited: &[String]) -> bool {
+        if CaveMap::is_small_cave(dest) {
+            let small_was_visited_twice = visited
+                .iter()
+                .filter(|cave| CaveMap::is_small_cave(cave))
+                .map(|cave| visited.iter().filter(|c| *c == cave).count())
+                .any(|count| count > 1);
+            let num_seen = visited.iter().filter(|cave| *cave == dest).count();
+
+            if small_was_visited_twice || dest == "start" || dest == "end" {
+                num_seen == 0
+            } else {
+                num_seen < 2
+            }
+        } else {
+            true
+        }
+    }
+    cave.explore_paths("start", "end", &[], can_visit).len()
 }
 
 fn main() {
@@ -117,4 +143,8 @@ start-RW
     assert_eq!(19, part1(test2));
     assert_eq!(226, part1(test3));
     println!("{}", part1(include_str!("../input")));
+    assert_eq!(36, part2(test1));
+    assert_eq!(103, part2(test2));
+    assert_eq!(3509, part2(test3));
+    println!("{}", part2(include_str!("../input")));
 }
