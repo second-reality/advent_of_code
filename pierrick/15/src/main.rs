7dbee7462a5b1b3424ea63ebfdbb65144824ea8d
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2021-12-15 15:22:29 +0100

    part2

--- pierrick/15/src/main.rs
+++ pierrick/15/src/main.rs
@@ -10,11 +10,12 @@ struct Problem {
     end: NodeIndex,
 }
 
-fn get_input(s: &str) -> Problem {
+fn get_input(s: &str, num_duplicates: i32) -> Problem {
     let mut res = Graph::new();
     let mut nodes = HashMap::new();
 
-    let size = s.lines().next().unwrap().len() as i32;
+    let orig_size = s.lines().next().unwrap().len() as i32;
+    let size = orig_size * (num_duplicates + 1);
 
     for (i, row) in s.lines().enumerate() {
         for (j, val) in row.chars().map(|c| c.to_digit(10).unwrap()).enumerate() {
@@ -23,13 +24,27 @@ fn get_input(s: &str) -> Problem {
             let val = val as i32;
             let node = res.add_node(());
             nodes.insert((i, j), (node, val));
+            for x_scale in 0..num_duplicates + 1 {
+                for y_scale in 0..num_duplicates + 1 {
+                    if x_scale == 0 && y_scale == 0 {
+                        continue;
+                    }
+                    let dup_i = i + x_scale * orig_size;
+                    let dup_j = j + y_scale * orig_size;
+                    let mut dup_val = val + x_scale + y_scale;
+                    if dup_val > 9 {
+                        dup_val = dup_val % 10 + 1;
+                    }
+                    let node = res.add_node(());
+                    nodes.insert((dup_i, dup_j), (node, dup_val));
+                }
+            }
         }
     }
 
     for i in 0..size {
         for j in 0..size {
             let current = nodes.get(&(i, j)).unwrap().0;
-            // left
             [
                 nodes.get(&(i - 1, j)), // left
                 nodes.get(&(i + 1, j)), // right
@@ -51,7 +66,8 @@ fn get_input(s: &str) -> Problem {
     }
 }
 
-fn part1(p: &Problem) -> i32 {
+fn solution(s: &str, num_duplicates: i32) -> i32 {
+    let p = get_input(s, num_duplicates);
     let shortest_path = astar(
         &p.graph,
         p.start,
@@ -61,6 +77,14 @@ fn part1(p: &Problem) -> i32 {
     )
     .unwrap();
     shortest_path.0
+}
+
+fn part1(s: &str) -> i32 {
+    solution(s, 0)
+}
+
+fn part2(s: &str) -> i32 {
+    solution(s, 4)
 }
 
 const TEST: &str = "1163751742
@@ -75,8 +99,8 @@ const TEST: &str = "1163751742
 2311944581";
 
 fn main() {
-    let p = get_input(TEST);
-    assert_eq!(40, part1(&p));
-    let input = get_input(include_str!("../input"));
-    println!("{}", part1(&input));
+    assert_eq!(40, part1(TEST));
+    println!("{}", part1(include_str!("../input")));
+    assert_eq!(315, part2(TEST));
+    println!("{}", part2(include_str!("../input")));
 }
