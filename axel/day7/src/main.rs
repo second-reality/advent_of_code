00e413653015ba417a3076b7010c582d294b8b99
Axel Ricard <axel.ricard@allegrodvt.com>
2022-01-02 22:57:15 +0100

    day 7 part 2

--- axel/day7/src/main.rs
+++ axel/day7/src/main.rs
@@ -6,23 +6,39 @@ fn main() {
                     .collect::<Vec<i32>>();
     let max_val = input.iter().max().unwrap();
     let min_val = input.iter().min().unwrap();
-    let mut min_cost = i32::MAX;
+    let mut min_cost_part1 = i32::MAX;
+    let mut min_cost_part2 = i32::MAX;
 
     for offset in *min_val..*max_val {
-        let ccost = cost(&input, offset);
-        if ccost < min_cost {
-            min_cost = ccost;
+        let cost_part1 = cost_part1(&input, offset);
+        let cost_part2 = cost_part2(&input, offset);
+        if cost_part1 < min_cost_part1 {
+            min_cost_part1 = cost_part1;
+        }
+        if cost_part2 < min_cost_part2 {
+            min_cost_part2 = cost_part2;
         }
     }
 
-    println!("{}", min_cost);
+    println!("{}", min_cost_part1);
+    println!("{}", min_cost_part2);
 
 }
 
-fn cost(crab_positions: &Vec<i32>, offset: i32) -> i32 {
+fn cost_part1(crab_positions: &Vec<i32>, offset: i32) -> i32 {
     let mut cost = 0;
     for pos in crab_positions {
         cost += (pos - offset).abs();
+    }
+    
+    return cost;
+}
+
+fn cost_part2(crab_positions: &Vec<i32>, offset: i32) -> i32 {
+    let mut cost = 0;
+    for pos in crab_positions {
+        let n = (pos - offset).abs();
+        cost += n * (n + 1) / 2;
     }
     
     return cost;
