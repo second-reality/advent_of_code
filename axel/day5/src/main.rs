0c27976737fefe31b75b941503c1f17b4f132cb6
Axel Ricard <axel.ricard@allegrodvt.com>
2022-01-02 15:26:52 +0100

    day 5 part 2

--- axel/day5/src/main.rs
+++ axel/day5/src/main.rs
@@ -27,37 +27,88 @@ fn parse_input(input: &'static str) -> Vec<(usize, usize, usize, usize)> {
 
 
 fn part1(input: &Vec<(usize, usize, usize, usize)>) {
-    let mut diagram : [usize; MAX_VAL * MAX_VAL] = [0; MAX_VAL * MAX_VAL];
+    let mut diagram = vec![0; MAX_VAL * MAX_VAL];
+    let mut diagram_part2 = vec![0; MAX_VAL * MAX_VAL];
     for (x0, y0, x1, y1) in input {
-        update_diagram(&mut diagram, *x0, *y0, *x1, *y1);
+        update_diagram(&mut diagram, *x0, *y0, *x1, *y1, false);
+        update_diagram(&mut diagram_part2, *x0, *y0, *x1, *y1, true);
     }
 
-    let mut cmpt = 0;
+    let mut cmpt_part1 = 0;
+    let mut cmpt_part2 = 0;
     for cell in &diagram {
         if *cell > 1 {
-            cmpt += 1;
+            cmpt_part1 += 1;
         }
     }
-    println!("{}", cmpt);
+    for cell in &diagram_part2 {
+        if *cell > 1 {
+            cmpt_part2 += 1;
+        }
+    }
+    println!("{}", cmpt_part1);
+    println!("{}", cmpt_part2);
 }
 
-fn update_diagram(diagram: &mut [usize; MAX_VAL * MAX_VAL], x0: usize, y0: usize, x1: usize, y1: usize) {
-   if x0 != x1 && y0 == y1  { // horizontal
-       for coordinate in 0..(max(x0,x1) - min(x0, x1) + 1) {
+enum ORIENTATION { NW, NE, SW, SE }
+
+fn update_diagram(diagram: &mut Vec<usize>, x0: usize, y0: usize, x1: usize, y1: usize, is_part_two: bool) {
+    if x0 != x1 && y0 == y1  { // horizontal
+        for coordinate in 0..(max(x0,x1) - min(x0, x1) + 1) {
             diagram[coordinate + min(x0, x1) + y0 * MAX_VAL] += 1;
-       }
-   }
-   else if x0 == x1 && y0 != y1  { // vertical
+        }
+    }
+    else if x0 == x1 && y0 != y1  { // vertical
        for coordinate in 0..(max(y0, y1) - min(y0, y1) + 1) {
             diagram[x0 + (coordinate + min(y0, y1)) * MAX_VAL] += 1;
-       }
-   }
+        }
+    }
+
+    else if is_part_two {
+        let diag_orientation : ORIENTATION;
+        if x0 < x1 && y0 < y1 {
+            diag_orientation = ORIENTATION::SW;
+        } else if x0 > x1 && y0 > y1 {
+            diag_orientation = ORIENTATION::NE;
+        } else if x0 < x1 && y0 > y1 {
+            diag_orientation = ORIENTATION::NW;
+        } else if x0 > x1 && y0 < y1 {
+            diag_orientation = ORIENTATION::SE;
+        } else {
+            panic!("shouldn't be here");
+        }
+
+        let offset = max(x0, x1) - min(x0, x1) + 1;
+        match diag_orientation {
+            ORIENTATION::SW => {
+                for i in 0..offset {
+                    diagram[x0 + i + (y0 + i) * MAX_VAL] += 1;
+                }
+            }
+            ORIENTATION::SE => {
+                for i in 0..offset {
+                    diagram[x0 - i + (y0 + i) * MAX_VAL] += 1;
+                }
+            }
+            ORIENTATION::NW => {
+                for i in 0..offset {
+                    diagram[x0 + i + (y0 - i) * MAX_VAL] += 1;
+                }
+            }
+            ORIENTATION::NE => {
+                for i in 0..offset {
+                    diagram[x0 - i + (y0 - i) * MAX_VAL] += 1;
+                }
+            }
+        }
+    }
+
 }
 
 #[allow(dead_code)]
-fn print_2d_array(array: &[usize; MAX_VAL * MAX_VAL]) {
-    for i in 0..MAX_VAL {
-        for j in 0..MAX_VAL {
+fn print_2d_array(array: &Vec<usize>, dim: usize) {
+    for i in 0..dim {
+        for j in 0..dim {
             print!("{}, ", array[j + i * MAX_VAL]);
         }
         println!("");
