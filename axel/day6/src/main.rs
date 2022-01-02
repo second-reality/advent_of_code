ed0ac57aa5866793734bf920038459cc707433d6
Axel Ricard <axel.ricard@allegrodvt.com>
2022-01-02 22:38:01 +0100

    day 6 part 2 with memoization

--- axel/day6/src/main.rs
+++ axel/day6/src/main.rs
@@ -1,26 +1,33 @@
-extern crate rayon;
-use rayon::prelude::*;
-
-const NUM_OF_DAYS : i64 = 256;
+const NUM_OF_DAYS : usize = 257;
 
 fn main() {
     let input = include_str!("../input").trim();
+
+    let mut tab = vec![-1; NUM_OF_DAYS * 9];
+
     let initial_number_of_fishes = input.split(",").count() as i64;
     let total = input.split(",")
                      .map(|c| c.parse::<i64>().unwrap())
-                     .map(|i| number_of_fishes(i, NUM_OF_DAYS))
+                     .map(|i| number_of_fishes(&mut tab, i, NUM_OF_DAYS-1))
                      .sum::<i64>();
+    println!("total : {}", total);
     println!("{:#?}", total + initial_number_of_fishes);
+    //print_2d_array(&tab, 9, NUM_OF_DAYS);
 }
 
-fn number_of_fishes(fish_timer: i64, days_remaining: i64) -> i64 {
-    if fish_timer >= days_remaining {
-        0
+fn number_of_fishes(tab: &mut Vec<i64>, fish_timer: i64, days_remaining: usize) -> i64 {
+    if tab[fish_timer as usize + days_remaining * 9] != -1 {
+        return tab[fish_timer as usize + days_remaining * 9];
+    }
+    
+    if fish_timer >= days_remaining as i64 {
+        tab[fish_timer as usize + days_remaining * 9] = 0;
+        return 0;
     } else {
-        let (a,b) = rayon::join(
-            || number_of_fishes(7, days_remaining - fish_timer),
-            || number_of_fishes(9, days_remaining - fish_timer));
-        1 + a + b
+        let a = number_of_fishes(tab, 7, days_remaining - (fish_timer as usize));
+        let b = number_of_fishes(tab, 9, days_remaining - (fish_timer as usize));
+        tab[fish_timer as usize + days_remaining * 9] = 1 + a + b;
+        return tab[fish_timer as usize + days_remaining * 9];
     }
 }
 
