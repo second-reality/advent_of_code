d584c72a17381fbd3997bb5389d02e2eedead426
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2022-11-23 11:58:30 +0100

    do not silent build

--- 2021/pierrick/run_all.sh
+++ 2021/pierrick/run_all.sh
@@ -9,5 +9,5 @@ export TIMEFORMAT="%R seconds" # elapsed
 for day in */; do
   day=$(echo $day | sed -e 's#/##')
   echo "******** DAY $day *******"
-  cd $day && cargo build --release -q && time ./target/release/ex$day -q && cd ..
+  cd $day && cargo build --release && time ./target/release/ex$day -q && cd ..
 done
