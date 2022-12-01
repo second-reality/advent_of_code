8503067a6902056f5abaffe6ed86e1d8086a0d04
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2022-12-01 10:00:39 +0100

    pierrick - execute days in reverse order

--- 2022/pierrick/run_all.sh
+++ 2022/pierrick/run_all.sh
@@ -6,8 +6,8 @@ cd $(readlink -f $(dirname $0))
 
 export TIMEFORMAT="%R seconds" # elapsed
 
-for day in */; do
-  day=$(echo $day | sed -e 's#/##')
+for day in $(find -maxdepth 1 -mindepth 1 -type d | sort -r); do
+  day=$(echo $day | sed -e 's#./##')
   echo "******** DAY $day *******"
   pushd $day > /dev/null
   cargo fmt
