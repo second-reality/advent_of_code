c9a7dd3bb6f89483c088e08e7f8b0a417bb45195
Valentin Trophime <valentin.trophime@inria.fr>
2022-12-01 11:29:50 +0100

    template done + day 1

--- 2022/valentin/src/day01.rs
+++ 2022/valentin/src/day01.rs
@@ -1,15 +1,36 @@
-pub fn part1(_input: String) -> usize {
-    42
+pub fn part1(input: String) -> usize {
+    input
+        .trim()
+        .split("\n\n")
+        .map(|elf| {
+            elf.split("\n")
+                .map(|calories| calories.parse::<usize>().unwrap())
+                .sum()
+        })
+        .max()
+        .unwrap()
 }
 
-pub fn part2(_input: String) -> usize {
-    42
+pub fn part2(input: String) -> usize {
+    let mut total_calories: Vec<usize> = input
+        .trim()
+        .split("\n\n")
+        .map(|elf| {
+            elf.split("\n")
+                .map(|calories| calories.parse::<usize>().unwrap())
+                .sum()
+        })
+        .collect();
+
+    total_calories.sort();
+    total_calories.reverse();
+    total_calories.into_iter().take(3).sum()
 }
 
 pub fn test1(example: String) {
-    assert_eq!(part1(example), 42);
+    assert_eq!(part1(example), 24000);
 }
 
 pub fn test2(example: String) {
-    assert_eq!(part2(example), 42);
+    assert_eq!(part2(example), 45000);
 }
--- 2022/valentin/src/main.rs
+++ 2022/valentin/src/main.rs
@@ -1,10 +1,52 @@
+use std::env;
+use std::fs;
 use val2022aoc::get_day;
 
 fn main() {
-    println!("Hello, world!");
-    println!(
-        "Day {}, Solution = {}",
-        1,
-        get_day(1).0 .0(String::default())
-    );
+    // Get day from cli argument
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Expected 1 argument for day number (1-25)");
+        return;
+    }
+
+    let day: u32 = args[1].parse().expect("Invalid argument, not a number");
+
+    // catch the code to test and run
+    let ((part1, test1), (part2, test2)) = get_day(day);
+
+    // get example input to do quick safe checks
+    let path_example = env::current_dir()
+        .unwrap()
+        .join("examples")
+        .join(format!("{:02}.txt", day));
+    let example = fs::read_to_string(path_example).expect("Example file not found");
+
+    // testing part 1
+    println!("Testing Part 1...");
+    test1(example.clone());
+    println!("Part 1 test is ok !");
+
+    // get input
+    let path_input = env::current_dir()
+        .unwrap()
+        .join("inputs")
+        .join(format!("{:02}.txt", day));
+    let input = fs::read_to_string(path_input).expect("Input file not found");
+
+    // run part 1
+    println!("Running Part 1...");
+    let answer1 = part1(input.clone());
+    println!("Part 1 answer is {}", answer1);
+
+    // test part 2
+    println!("Testing Part 2...");
+    test2(example);
+    println!("Part 2 test is ok !");
+
+    // run part 2
+    println!("Running Part 2...");
+    let answer2 = part2(input);
+    println!("Part 2 answer is {}", answer2);
 }
