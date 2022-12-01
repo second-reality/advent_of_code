ed935188bdb0803909a82b51cfcf6c488ac4a85d
Valentin Trophime <valentin.trophime@inria.fr>
2022-12-01 18:53:57 +0100

    less duplicate, add day number inside prints

--- 2022/valentin/src/main.rs
+++ 2022/valentin/src/main.rs
@@ -1,47 +1,54 @@
 use std::env;
 use std::fs;
 use std::time::Instant;
-use val2022aoc::get_day;
+use val2022aoc::{get_day, PartImplem};
+
+fn time_part(part_func: PartImplem, input: String, day: u32, part: u8) {
+    let start = Instant::now();
+    let output = part_func(input);
+    let duration = start.elapsed();
+    println!(
+        "Day {}, Part {} answer = {}, took {:?}",
+        day, part, output, duration
+    );
+}
+
+fn get_string_from(day: u32, is_example: bool) -> String {
+    let (dir, err_msg) = if is_example {
+        ("examples", "Example file missing!")
+    } else {
+        ("inputs", "Input file missing!")
+    };
+    let path = env::current_dir()
+        .unwrap()
+        .join(dir)
+        .join(format!("{:02}.txt", day));
+    fs::read_to_string(path).expect(err_msg)
+}
 
 fn exec_and_time_day(day: u32) {
     // catch the code to test and run
     let ((part1, test1), (part2, test2)) = get_day(day);
 
-    // get example input to do quick safe checks
-    let path_example = env::current_dir()
-        .unwrap()
-        .join("examples")
-        .join(format!("{:02}.txt", day));
-    let example = fs::read_to_string(path_example).expect("Example file not found");
-    // testing part 1
-    println!("Testing Part 1...");
-    test1(example.clone());
-    println!("Part 1 test is ok !");
+    // get example
+    let example = get_string_from(day, true);
 
     // get input
-    let path_input = env::current_dir()
-        .unwrap()
-        .join("inputs")
-        .join(format!("{:02}.txt", day));
-    let input = fs::read_to_string(path_input).expect("Input file not found");
-    // run part 1
-    println!("Running Part 1...");
-    let start = Instant::now();
-    let answer = part1(input.clone());
-    let duration = start.elapsed();
-    println!("Part 1 answer is {}, takes {:?}", answer, duration);
+    let input = get_string_from(day, false);
+
+    // testing part 1
+    test1(example.clone());
+    println!("Day {}, Part 1 test passed !", day);
+
+    // run and time part 1
+    time_part(part1, input.clone(), day, 1);
 
     // test part 2
-    println!("Testing Part 2...");
     test2(example);
-    println!("Part 2 test is ok !");
+    println!("Day {}, Part 2 test passed !", day);
 
     // run part 2
-    println!("Running Part 2...");
-    let start = Instant::now();
-    let answer = part2(input);
-    let duration = start.elapsed();
-    println!("Part 2 answer is {}, takes {:?}", answer, duration);
+    time_part(part2, input, day, 2);
 }
 
 fn main() {
@@ -49,8 +56,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        println!("Expected 1 argument for day number (1-25)");
-        return;
+        panic!("Expected 1 argument for day number (e.g cargo run 12)");
     }
 
     match args[1].as_str() {
@@ -60,7 +66,9 @@ fn main() {
             }
         }
         arg => {
-            let day: u32 = arg.parse().unwrap();
+            let day: u32 = arg
+                .parse()
+                .expect("Invalid argument should be number (e.g cargo run 12)");
             exec_and_time_day(day);
         }
     }
