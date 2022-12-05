209bd71f506badbf013d98c17870360559864b10
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2022-12-05 10:54:24 +0100

    pierrick - add template

---
+++ 2022/pierrick/template.rs
@@ -0,0 +1,25 @@
+struct Input {
+}
+
+fn input(input_str: &str) -> Input {
+    todo!();
+}
+
+fn part1(i: &Input) -> usize {
+    todo!();
+}
+
+fn part2(i: &Input) -> usize {
+    todo!();
+}
+
+fn main() {
+    //let test_str = include_str!("../test");
+    //let t = input(test_str);
+    //assert_eq!(part1(&t), 0);
+    //assert_eq!(part2(&t), 0);
+    let i = input(input_str);
+    let input_str = include_str!("../input");
+    println!("{}", part1(&i));
+    println!("{}", part2(&i));
+}
