26d497977f58f1cd9e89ce3770e32d6afa90e601
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2022-12-06 09:13:29 +0100

    pierrick - 06

---
+++ 2022/pierrick/06/src/main.rs
@@ -0,0 +1,37 @@
+type Input = Vec<char>;
+
+fn input(input_str: &str) -> Input {
+    input_str.chars().collect()
+}
+
+fn find_marker(i: &Input, win_size: usize) -> usize {
+    i.windows(win_size)
+        .position(|w| {
+            let mut chars = w.to_vec();
+            chars.sort();
+            chars.dedup();
+            chars.len() == win_size
+        })
+        .unwrap()
+        + win_size
+}
+
+fn part1(i: &Input) -> usize {
+    find_marker(i, 4)
+}
+
+fn part2(i: &Input) -> usize {
+    find_marker(i, 14)
+}
+
+fn main() {
+    //let test_str = include_str!("../test");
+    //let t = input(test_str);
+    //assert_eq!(part1(&t), 0);
+    //assert_eq!(part2(&t), 0);
+    let input_str = include_str!("../input");
+    let i = input(input_str);
+    assert_eq!(part1(&input("bvwbjplbgvbhsrlpgdmjqwftvncz")), 5);
+    println!("{}", part1(&i));
+    println!("{}", part2(&i));
+}
