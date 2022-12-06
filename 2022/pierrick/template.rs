b5fa9ae458bc85b92041ee5458d76593fdf932c2
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2022-12-06 09:07:29 +0100

    fix template

--- 2022/pierrick/template.rs
+++ 2022/pierrick/template.rs
@@ -18,8 +18,8 @@ fn main() {
     //let t = input(test_str);
     //assert_eq!(part1(&t), 0);
     //assert_eq!(part2(&t), 0);
-    let i = input(input_str);
     let input_str = include_str!("../input");
+    let i = input(input_str);
     println!("{}", part1(&i));
     println!("{}", part2(&i));
 }
