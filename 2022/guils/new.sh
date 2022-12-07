554ca9683b5f8b9ebad9ca41e3fa0878b097e71b
Christophe Guillon <christophe.guillon@inria.fr>
2022-12-07 22:54:34 +0100

    guils - harness

--- 2022/guils/new.sh
+++ 2022/guils/new.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 day="${1?}"
 
-cd "$($dirname "$0")"
-cargo new "$day"
-cp -pr template/* "$day"/
+cd "$(dirname "$0")"
+cargo new "day$day"
+cp -pr template/* "day$day"/
+git add "day$day"/
+git status -uno
+git commit -m "guils - $day"
