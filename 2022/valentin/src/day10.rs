1f0629b8e50b29b4baf73f07efa70a99a0b139bc
Valentin Trophime <theval358@gmail.com>
2022-12-11 13:44:53 +0100

    valentin day 10 done

--- 2022/valentin/src/day10.rs
+++ 2022/valentin/src/day10.rs
@@ -1,10 +1,55 @@
-pub fn part1(_input: String) -> usize {
+pub fn part1(input: String) -> usize {
+    let mut register = 1;
+    let mut cycle = 1;
+    let mut res = 0;
+    for line in input.trim().lines() {
+        if (cycle - 20) % 40 == 0 {
+            res += register * cycle;
+        }
+        let addx = line.split(' ').last().unwrap().parse::<i32>();
+        cycle += if let Ok(increment) = addx {
+            if (cycle + 1 - 20) % 40 == 0 {
+                res += register * (cycle + 1);
+            }
+            register += increment;
+            2
+        } else {
+            1
+        }
+    }
+    res as usize
+}
+
+fn update_crt(crt: &mut String, cycle: i32, x: i32) {
+    let pos = (cycle - 1) % 40;
+    let pixel = if pos - 1 <= x && x <= pos + 1 {
+        '#'
+    } else {
+        '.'
+    };
+    crt.push(pixel);
+    if cycle % 40 == 0 {
+        crt.push('\n');
+    }
+}
+
+pub fn part2(input: String) -> usize {
+    let mut register = 1;
+    let mut cycle = 1;
+    let mut res = String::new();
+    for line in input.trim().lines() {
+        update_crt(&mut res, cycle, register);
+        let addx = line.split(' ').last().unwrap().parse::<i32>();
+        if let Ok(increment) = addx {
+            cycle += 1;
+            update_crt(&mut res, cycle, register);
+            register += increment;
+        }
+        cycle += 1;
+    }
+    println!("{}", res);
     42
 }
 
-pub fn part2(_input: String) -> usize {
-    42
-}
-
-pub const EXPECTED1: usize = 42;
+pub const EXPECTED1: usize = 13_140;
 pub const EXPECTED2: usize = 42;
