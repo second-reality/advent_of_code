71c4edca67a20ef42c49dee33a0fa88f7360df84
Valentin Trophime <theval358@gmail.com>
2022-12-17 22:47:00 +0100

    valentin day 14 and 15 done

--- 2022/valentin/src/day14.rs
+++ 2022/valentin/src/day14.rs
@@ -1,10 +1,116 @@
-pub fn part1(_input: String) -> usize {
-    42
+use std::{collections::HashSet, str::FromStr};
+
+type Coord = (i32, i32);
+trait FallCoord {
+    fn diag_left(&self) -> Self;
+    fn diag_right(&self) -> Self;
+    fn down(&self) -> Self;
+    fn from_input_str(s: &str) -> Self;
+    fn range_inclusive(self, other: &Self) -> Vec<Coord>;
 }
 
-pub fn part2(_input: String) -> usize {
-    42
+impl FallCoord for Coord {
+    fn diag_left(&self) -> Self {
+        (self.0 - 1, self.1 + 1)
+    }
+
+    fn diag_right(&self) -> Self {
+        (self.0 + 1, self.1 + 1)
+    }
+
+    fn down(&self) -> Self {
+        (self.0, self.1 + 1)
+    }
+    fn from_input_str(s: &str) -> Self {
+        let mut tmp = s.split(',');
+        (
+            tmp.next().unwrap().parse().unwrap(),
+            tmp.next().unwrap().parse().unwrap(),
+        )
+    }
+
+    fn range_inclusive(self, other: &Self) -> Vec<Coord> {
+        if self.0 == other.0 {
+            let (mn, mx) = (self.1.min(other.1), self.1.max(other.1));
+            (mn..=mx).map(|y| (self.0, y)).collect()
+        } else {
+            let (mn, mx) = (self.0.min(other.0), self.0.max(other.0));
+            (mn..=mx).map(|x| (x, self.1)).collect()
+        }
+    }
 }
 
-pub const EXPECTED1: usize = 42;
-pub const EXPECTED2: usize = 42;
+const SAND_SOURCE: Coord = (500, 0);
+
+#[derive(Debug)]
+struct Cave {
+    walls: HashSet<Coord>,
+    lowest_wall: i32,
+}
+
+enum FallingState {
+    Finished(Coord),
+    Endless,
+    Error,
+}
+
+impl Cave {
+    fn fill(&self, sand_pos: Coord) -> FallingState {
+        if self.walls.contains(&sand_pos) {
+            FallingState::Error
+        } else if sand_pos.1 > self.lowest_wall {
+            FallingState::Endless
+        } else {
+            let to_try = [sand_pos.down(), sand_pos.diag_left(), sand_pos.diag_right()];
+            to_try
+                .into_iter()
+                .find_map(|pos| match self.fill(pos) {
+                    FallingState::Error => None,
+                    other => Some(other),
+                })
+                .unwrap_or(FallingState::Finished(sand_pos))
+        }
+    }
+    fn fill_until_end(&mut self) -> usize {
+        let mut res = 0;
+        while let FallingState::Finished(new_wall) = self.fill(SAND_SOURCE) {
+            self.walls.insert(new_wall);
+            res += 1;
+        }
+        res
+    }
+}
+impl FromStr for Cave {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut walls: HashSet<Coord> = HashSet::new();
+        for line in s.split('\n') {
+            let mut points = line.split(" -> ").map(|s| Coord::from_input_str(s));
+            let mut prev = points.next().unwrap();
+            for p in points {
+                let range = prev.range_inclusive(&p);
+                walls.extend(range);
+                prev = p;
+            }
+        }
+        let lowest_wall = *walls.iter().map(|(_, y)| y).max().unwrap();
+        Ok(Cave { walls, lowest_wall })
+    }
+}
+pub fn part1(input: String) -> usize {
+    let mut cave: Cave = input.trim().parse().unwrap();
+    cave.fill_until_end()
+}
+
+pub fn part2(input: String) -> usize {
+    let mut cave: Cave = input.trim().parse().unwrap();
+    cave.lowest_wall += 2;
+    let min_x = SAND_SOURCE.0 - cave.lowest_wall - 1;
+    let max_x = SAND_SOURCE.0 + cave.lowest_wall + 1;
+    cave.walls
+        .extend((min_x..=max_x).map(|x| (x, cave.lowest_wall)));
+    cave.fill_until_end()
+}
+
+pub const EXPECTED1: usize = 24;
+pub const EXPECTED2: usize = 93;
--- 2022/valentin/src/day15.rs
+++ 2022/valentin/src/day15.rs
@@ -1,10 +1,182 @@
-pub fn part1(_input: String) -> usize {
-    42
+use std::{
+    num::ParseIntError,
+    ops::{Add, Mul, RangeInclusive},
+    str::FromStr,
+};
+#[derive(PartialEq, Clone, Copy)]
+struct Coord {
+    x: i32,
+    y: i32,
 }
 
-pub fn part2(_input: String) -> usize {
-    42
+impl FromStr for Coord {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut tmp = s
+            .split(' ')
+            .filter(|sp| sp.contains('='))
+            .map(|sp| sp.split('=').last().unwrap());
+        let x: i32 = tmp.next().unwrap().replace(",", "").parse()?;
+        let y: i32 = tmp.next().unwrap().parse()?;
+        Ok(Coord { x, y })
+    }
 }
 
-pub const EXPECTED1: usize = 42;
-pub const EXPECTED2: usize = 42;
+impl Coord {
+    fn manhattan_dist(&self, other: &Self) -> i32 {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+}
+
+impl Add for Coord {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Coord {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Mul<i32> for Coord {
+    type Output = Self;
+    fn mul(self, rhs: i32) -> Self::Output {
+        Coord {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+struct Circles {
+    center: Coord,
+    radius: i32,
+}
+impl Circles {
+    fn intersection_with_y(&self, y: i32) -> Option<RangeInclusive<i32>> {
+        let delta = self.radius - (y - self.center.y).abs();
+        if delta >= 0 {
+            Some((self.center.x - delta)..=(self.center.x + delta))
+        } else {
+            None
+        }
+    }
+
+    fn contains(&self, point: &Coord) -> bool {
+        self.center.manhattan_dist(point) <= self.radius
+    }
+}
+fn union(intervals: &mut Vec<RangeInclusive<i32>>, range2: RangeInclusive<i32>) {
+    let mut min = i32::MAX;
+    let mut max = i32::MIN;
+    let to_remove: Vec<usize> = intervals
+        .iter()
+        .enumerate()
+        .filter_map(|(i, range1)| {
+            if range1.start() <= range2.end() && range1.end() >= range2.start() {
+                min = *range1.start().min(range2.start()).min(&min);
+                max = *range1.end().max(range2.end()).max(&max);
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .collect();
+    let add = if to_remove.is_empty() {
+        range2
+    } else {
+        to_remove.into_iter().rev().for_each(|i| {
+            intervals.remove(i);
+        });
+        min..=max
+    };
+    intervals.push(add);
+}
+
+fn parse_input(input: String, y_to_check: i32) -> (Vec<Circles>, Vec<Coord>) {
+    let mut beacons_to_check = Vec::new();
+    let res = input
+        .trim()
+        .split('\n')
+        .map(|line| {
+            let mut halfs = line.split(':');
+            let scanner: Coord = halfs.next().unwrap().parse().unwrap();
+            let beacon: Coord = halfs.next().unwrap().parse().unwrap();
+            let dist = scanner.manhattan_dist(&beacon);
+            if beacon.y == y_to_check && !beacons_to_check.contains(&beacon) {
+                beacons_to_check.push(beacon)
+            }
+            Circles {
+                center: scanner,
+                radius: dist,
+            }
+        })
+        .collect();
+    (res, beacons_to_check)
+}
+
+pub fn part1(input: String) -> usize {
+    let y_to_check = if input.len() < 800 {
+        // example
+        10
+    } else {
+        2_000_000
+    };
+    let (circles, beacons) = parse_input(input, y_to_check);
+    let mut ranges_x: Vec<RangeInclusive<i32>> = Vec::new();
+    circles
+        .iter()
+        .filter_map(|circle| circle.intersection_with_y(y_to_check))
+        .for_each(|range| union(&mut ranges_x, range));
+    let beacon_ytc: usize = beacons
+        .into_iter()
+        .filter(|b| ranges_x.iter().any(|range| range.contains(&b.x)))
+        .count();
+    let count: usize = ranges_x.into_iter().map(|interval| interval.count()).sum();
+    count - beacon_ytc
+}
+
+const LEFT: Coord = Coord { x: -1, y: 0 };
+const RIGHT: Coord = Coord { x: 1, y: 0 };
+const UP: Coord = Coord { x: 0, y: -1 };
+const DOWN: Coord = Coord { x: 0, y: 1 };
+
+const DIAG_UP_LEFT: Coord = Coord { x: -1, y: -1 };
+const DIAG_UP_RIGHT: Coord = Coord { x: 1, y: -1 };
+const DIAG_DOWN_RIGHT: Coord = Coord { x: 1, y: 1 };
+const DIAG_DOWN_LEFT: Coord = Coord { x: -1, y: 1 };
+
+pub fn part2(input: String) -> usize {
+    let len_search = if input.len() < 800 {
+        // example
+        20
+    } else {
+        4_000_000
+    };
+    let (circles, _) = parse_input(input, 0);
+
+    let point = circles
+        .iter()
+        .find_map(|circle| {
+            let r = circle.radius + 1;
+            let mut contour = (0..=circle.radius).flat_map(|i| {
+                [
+                    circle.center + (UP * r) + (DIAG_DOWN_RIGHT * i),
+                    circle.center + (RIGHT * r) + (DIAG_DOWN_LEFT * i),
+                    circle.center + (DOWN * r) + (DIAG_UP_LEFT * i),
+                    circle.center + (LEFT * r) + (DIAG_UP_RIGHT * i),
+                ]
+            });
+            contour.find(|point| {
+                (0..=len_search).contains(&point.x)
+                    && (0..=len_search).contains(&point.y)
+                    && circles.iter().all(|c| !c.contains(point))
+            })
+        })
+        .unwrap();
+    let (x, y) = (point.x as usize, point.y as usize);
+    x * 4_000_000 + y
+}
+
+pub const EXPECTED1: usize = 26;
+pub const EXPECTED2: usize = 56000011;
